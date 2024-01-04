8ec7a22ac2c480713c67be14115c330ef090b834
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-01-04 15:35:46 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-"""
-This module contains a method that determines if a given data set
-is valid UTF-8 encoding.
-"""
+""" UTF-8 Validation """
+
 
 def validUTF8(data):
     """
-    Determine if a given data set is valid UTF-8 encoding.
-    - Returns: True if data is valid UTF-8 encoding, False otherwise.
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    number_of_bytes = 0
-    mask = 255
+    number_bytes = 0
 
-    for byte in data:
-        if number_of_bytes == 0:
-            byte = byte & mask
-            if (byte >> 5) == 0b110:
-                number_of_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                number_of_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                number_of_bytes = 3
-            elif (byte >> 7):
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            if (byte >> 6) != 0b10:
-                return False
-            number_of_bytes -= 1
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-    return number_of_bytes == 0
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
